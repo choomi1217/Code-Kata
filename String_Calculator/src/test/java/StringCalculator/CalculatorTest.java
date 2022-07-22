d3cc9f18d17058b6b6918b1cec3bb3bf5c1ea7bb
KimWanJoo <wjwan0915@gmail.com>
2022-07-22 09:19:10 +0900

    test: 계산 테스트 구현

--- String_Calculator/src/test/java/StringCalculator/CalculatorTest.java
+++ String_Calculator/src/test/java/StringCalculator/CalculatorTest.java
@@ -1,49 +1,27 @@
 package StringCalculator;
 
-import StringCalulator.Operator;
-import org.junit.jupiter.api.Test;
+import StringCalulator.ArithmeticExpression;
+import StringCalulator.Calculator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class CalculatorTest {
-
-    @Test
-    void 더하기_연산() {
-        Operator plus = Operator.findBy('+');
-
-        double actual = plus.apply(3, 10);
-
-        assertThat(actual).isEqualTo(13.0);
-    }
-
-    @Test
-    void 빼기_연산() {
-        Operator minus = Operator.findBy('-');
-
-        double actual = minus.apply(3, 10);
-        //망주님 수고해써요~ ☺ 좋은밤 되세요!
-        assertThat(actual).isEqualTo(-7.0);
-    }
+class CalculatorTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {
-            "3 + 5",
-    })
-    void 곱하기_연산() {
-
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "3 + 5",
-            "13 + 5 - 4",
-            "63 + 5 / 2",
-            "1233 + 5 * 3 / 3",
-            "53 - 661",
-    })
-    void 나누기_연산() {
-
+    @CsvSource(value = {
+            "3 + 5:8",
+            "13 + 5 - 4:14",
+            "63 + 5 / 2:34",
+            "1233 + 5 * 3 / 3:1238",
+            "53 - 661:-608",
+            " 8 + 1 :9",
+            " 9 / 2 *3 :13.5"
+    }, delimiter = ':')
+    void 계산_테스트(String args, double result) {
+        ArithmeticExpression arithmeticExpression = new ArithmeticExpression(args);
+        Calculator calculator = new Calculator(arithmeticExpression.getStackArray());
+        double actual = calculator.calculation();
+        Assertions.assertThat(actual).isEqualTo(result);
     }
 }
