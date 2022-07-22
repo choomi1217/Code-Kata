a059e4779114975d765d0d6bdb79a2677f51103c
KimWanJoo <wjwan0915@gmail.com>
2022-07-22 09:19:52 +0900

    feat: 비즈니스 로직 구현 중

---
+++ String_Calculator/src/main/java/StringCalulator/Calculator.java
@@ -0,0 +1,22 @@
+package StringCalulator;
+
+import java.util.ArrayDeque;
+
+public class Calculator {
+
+    private final ArrayDeque<String> accumulate;
+
+    public Calculator(ArrayDeque<String> accumulate) {
+        this.accumulate = accumulate;
+    }
+
+    public double calculation() {
+
+        int count = 0;
+        double sum = 0;
+        while (!accumulate.isEmpty()) {
+
+        }
+        return 0;
+    }
+}
