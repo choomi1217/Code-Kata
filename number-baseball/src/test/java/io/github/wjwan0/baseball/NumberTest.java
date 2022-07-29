3b59654b4d437882fc1852b488709ba6544b5975
KimWanJoo <wjwan0915@gmail.com>
2022-07-29 23:13:24 +0900

    package convention rename

--- number-baseball/src/test/java/io/github/wjwan0/baseball/NumberTest.java
+++ number-baseball/src/test/java/io/github/wjwan0/baseball/NumberTest.java
@@ -1,4 +1,4 @@
-package number_baseball;
+package io.github.wjwan0.baseball;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -6,42 +6,28 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NumberTest {
-    @Test
-    void 중복되는_숫자가_있으면_예외처리를_한다() {
-        assertThatThrownBy(() -> Numbers.of("112"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {"12", "1234", "12 "})
     void mustBe3Numbers(String testcase) {
-        assertThatThrownBy(() -> Numbers.of(testcase))
+        assertThatThrownBy(() -> Numbers.specificNumbers(testcase))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 중복되지않는_숫자_세자리가_들어갔을_경우_예외를_발생하지_않는다() {
-        assertThatCode(() -> Numbers.of("123"))
+        assertThatCode(() -> Numbers.specificNumbers("123"))
                 .doesNotThrowAnyException();
     }
 
     @Test
-    void HastSET_중복제거_테스트() {
-        Set<Integer> set = new HashSet<>();
-        
-        set.add(1);
-        set.add(1);
-        set.add(2);
-
-        Assertions.assertThat(set.size()).isEqualTo(2);
+    void 랜덤숫자_만들기_테스트() {
+        Numbers randomNumbers = Numbers.randomNumbers();
+        Assertions.assertThat(randomNumbers.threeNumbers()).hasSize(3);
     }
 }
-
