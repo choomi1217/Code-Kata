8c90390343bf1e44837d5215c7f5931774996484
KimWanJoo <wjwan0915@gmail.com>
2022-08-03 22:05:37 +0900

    숫자 만들기 테스트 및 구현

---
+++ number-baseball-second-edition/src/main/java/io/github/wjwan0/o_omi/baseball/Ball.java
@@ -0,0 +1,16 @@
+package io.github.wjwan0.o_omi.baseball;
+
+public record Ball(int number) {
+    // 생성자, Getter, Hashcode, equals(?)
+
+    public Ball {
+        ballValidationCheck(number);
+    }
+
+    //구현만
+    private void ballValidationCheck(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(" 양수 값만 입력 해주세요. ");
+        }
+    }
+}
