bc3ee5090326ee8736eede18284940ff5c098845
KimWanJoo <wjwan0915@gmail.com>
2022-08-17 21:31:34 +0900

    :recycle: ConsoleOut Refactoring

--- car-racing/src/main/java/io/github/wjwan0/o_omi/car_racing/out/ConsoleOut.java
+++ car-racing/src/main/java/io/github/wjwan0/o_omi/car_racing/out/ConsoleOut.java
@@ -1,6 +1,9 @@
 package io.github.wjwan0.o_omi.car_racing.out;
 
+import io.github.wjwan0.o_omi.car_racing.Car;
 import io.github.wjwan0.o_omi.car_racing.Cars;
+
+import java.util.List;
 
 public class ConsoleOut {
 
@@ -10,5 +13,21 @@ public class ConsoleOut {
         System.out.println();
     }
 
+    public void resultOut(List<Car> winnerCarList){
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < winnerCarList.size(); i++) {
+            makeWinnerOutput(winnerCarList, result, i);
+        }
+
+        System.out.println(result);
+    }
+
+    private void makeWinnerOutput(List<Car> winnerList, StringBuilder result, int last) {
+        if (last != winnerList.size() - 1) {
+            result.append(winnerList.get(last).getCarName()).append(", ");
+            return;
+        }
+        result.append(winnerList.get(last).getCarName()).append("가 최종우승 했습니다.");
+    }
 
 }
