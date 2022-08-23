f65e61c48eeddaa79520215fafa286566c6172ea
KimWanJoo <wjwan0915@gmail.com>
2022-08-23 22:32:05 +0900

    :white_check_mark: rank test

--- car-racing/src/test/java/io/github/wjwan0/oomi/car/racing/CarsTest.java
+++ car-racing/src/test/java/io/github/wjwan0/oomi/car/racing/CarsTest.java
@@ -1,8 +1,12 @@
 package io.github.wjwan0.oomi.car.racing;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
 
 public class CarsTest {
 
@@ -15,6 +19,25 @@ public class CarsTest {
     void 입력한_이름_갯수_만큼_자동차_생성(String inputName, int count) {
         Cars cars = Cars.from(inputName);
         Assertions.assertThat(cars.getCarList().size()).isEqualTo(count);
+    }
+
+    @Test
+    @DisplayName("등수 확인 테스트")
+    void 등수_테스트() {
+        //given
+        Cars cars = Cars.from("완주,영미,거니");
+
+        //when
+        cars.run();
+        cars.run();
+        cars.run();
+        cars.run();
+        cars.run();
+        cars.run();
+
+        //then
+        List<Car> plyaerRanking = cars.getPlyaerRanking();
+        System.out.println("plyaerRanking = " + plyaerRanking.toString());
     }
 
 }
