679964c67b2c3419b0d1533c67f6ec6ac00471da
KimWanJoo <wjwan0915@gmail.com>
2022-08-30 00:37:34 +0900

    :sparkles: input 구현

---
+++ lotto/src/main/java/application/in/ConsoleIn.java
@@ -0,0 +1,20 @@
+package application.in;
+
+import java.util.Scanner;
+
+public class ConsoleIn {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public long inputMoney() {
+        return scanner.nextLong();
+    }
+
+    public String inputWinningNumbers() {
+        return scanner.next();
+    }
+
+    public int inputBonusBall() {
+        return scanner.nextInt();
+    }
+}
