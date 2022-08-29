6dd16342a6fc0675261e972d63ecff785ae7ec23
KimWanJoo <wjwan0915@gmail.com>
2022-08-29 23:49:12 +0900

    :white_check_mark: 로또 당첨 확인 테스트 수정

--- lotto/src/test/java/LottoCommitteeTests.java
+++ lotto/src/test/java/LottoCommitteeTests.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.EnumMap;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
