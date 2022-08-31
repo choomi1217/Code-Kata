89d7402879ff4510378db4aa3fdb4100dc56a165
KimWanJoo <wjwan0915@gmail.com>
2022-08-31 16:53:45 +0900

    :sparkles: 비즈니스 로직 구현

--- lotto/src/main/java/application/Game.java
+++ lotto/src/main/java/application/Game.java
@@ -2,12 +2,10 @@ package application;
 
 import application.in.ConsoleIn;
 import application.out.ConsoleOut;
-import domain.Lotto;
-import domain.LottoCommittee;
-import domain.LottoMachine;
-import domain.LottoNumber;
+import domain.*;
 
 import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
 
 public class Game {
@@ -18,22 +16,29 @@ public class Game {
     public void init() {
         consoleOut.buyLotto();
         LottoMachine lottoMachine = LottoMachine.from(1000);
-        List<Lotto> buyLottos = lottoMachine.sell(consoleIn.inputMoney());
+        long inputMoney = consoleIn.inputMoney();
+        List<Lotto> buyLottos = lottoMachine.sell(inputMoney);
         consoleOut.boughtLotto(buyLottos.size());
         buyLottos.forEach(consoleOut::printLotto);
         consoleOut.inputWinningNumber();
+
         String winningNumber = consoleIn.inputWinningNumbers();
         List<LottoNumber> lottoNumbers = Arrays.stream(winningNumber.split(","))
                 .map(Integer::parseInt)
                 .map(LottoNumber::from)
                 .toList();
+
         consoleOut.inputBonusBall();
         LottoNumber bonusBall = LottoNumber.from(consoleIn.inputBonusBall());
 
         LottoCommittee lottoCommittee = LottoCommittee.of(() -> Lotto.from(lottoNumbers), bonusBall);
 
-        consoleOut.statistics(lottoCommittee.matches(buyLottos));
+        EnumMap<Rank, Integer> matchLotto = lottoCommittee.matches(buyLottos);
+        consoleOut.statistics(matchLotto);
 
+        double totalMoney = Rank.totalMoney(matchLotto);
 
+        consoleOut.printYield(totalMoney / inputMoney);
+        consoleIn.close();
     }
 }
