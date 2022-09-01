bd1fdc0f55ae0d25dc11c217fe2c9abd4e2946f9
KimWanJoo <wjwan0915@gmail.com>
2022-09-01 23:54:51 +0900

    :sparkles: feat Card 및 CardDeck 클래스 구현 완료

---
+++ blackjack/src/main/java/io/github/wjwan0/oomi/blackjack/domain/CardDeck.java
@@ -0,0 +1,34 @@
+package io.github.wjwan0.oomi.blackjack.domain;
+
+import io.github.wjwan0.oomi.blackjack.domain.trump.Suit;
+import io.github.wjwan0.oomi.blackjack.domain.trump.TrumpNumber;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CardDeck {
+
+    private final ArrayDeque<Card> cardDeck;
+
+    private CardDeck(ArrayDeque<Card> cardDeck) {
+        this.cardDeck = new ArrayDeque<>(cardDeck);
+    }
+
+    public static CardDeck from() {
+        List<Card> cardDeck = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (TrumpNumber number : TrumpNumber.values()) {
+                cardDeck.add(Card.of(suit, number));
+            }
+        }
+        Collections.shuffle(cardDeck);
+
+        return new CardDeck(new ArrayDeque<>(cardDeck));
+    }
+
+    public ArrayDeque<Card> getCardDeck() {
+        return new ArrayDeque<>(cardDeck);
+    }
+}
