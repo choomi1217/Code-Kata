46d5444416777d5968f5a55a93bae6390d909623
KimWanJoo <wjwan0915@gmail.com>
2022-09-02 23:00:26 +0900

    :recycle: singleton 구현

--- blackjack/src/main/java/io/github/wjwan0/oomi/blackjack/domain/CardDeck.java
+++ blackjack/src/main/java/io/github/wjwan0/oomi/blackjack/domain/CardDeck.java
@@ -3,20 +3,26 @@ package io.github.wjwan0.oomi.blackjack.domain;
 import io.github.wjwan0.oomi.blackjack.domain.trump.Suit;
 import io.github.wjwan0.oomi.blackjack.domain.trump.TrumpNumber;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class CardDeck {
 
-    private final ArrayDeque<Card> cardDeck;
+    private static CardDeck deck = null;
+
+    private ArrayDeque<Card> cardDeck;
 
     private CardDeck(ArrayDeque<Card> cardDeck) {
         this.cardDeck = new ArrayDeque<>(cardDeck);
     }
 
     public static CardDeck from() {
+        if (deck == null) {
+            deck = new CardDeck(new ArrayDeque<>(makeCardDeck()));
+        }
+        return deck;
+    }
+
+    private static List<Card> makeCardDeck() {
         List<Card> cardDeck = new ArrayList<>();
         for (Suit suit : Suit.values()) {
             for (TrumpNumber number : TrumpNumber.values()) {
@@ -24,15 +30,14 @@ public class CardDeck {
             }
         }
         shuffle(cardDeck);
-
-        return new CardDeck(new ArrayDeque<>(cardDeck));
+        return cardDeck;
     }
 
     private static void shuffle(List<Card> cardDeck) {
         Collections.shuffle(cardDeck);
     }
 
-    public ArrayDeque<Card> getCardDeck() {
+    public ArrayDeque<Card> allCard() {
         return new ArrayDeque<>(cardDeck);
     }
 }
