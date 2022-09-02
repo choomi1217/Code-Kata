bf1f6d661abdeb30428acef2f851e39828d081f7
KimWanJoo <wjwan0915@gmail.com>
2022-09-02 23:00:17 +0900

    :white_check_mark: singleton 수정 테스트

--- blackjack/src/test/java/io/github/wjwan0/oomi/blackjack/domain/CardDeckTest.java
+++ blackjack/src/test/java/io/github/wjwan0/oomi/blackjack/domain/CardDeckTest.java
@@ -12,9 +12,27 @@ public class CardDeckTest {
     @DisplayName("전체 카드덱 생성 테스트")
     void cardDeckContructorTest() {
         assertDoesNotThrow(CardDeck::from);
-        CardDeck cardDeck = CardDeck.from();
-        assertEquals(cardDeck.getCardDeck().size(), 52);
+
     }
 
+    @Test
+    @DisplayName("카드 52장 확인")
+    void _52CardsCheck() {
+        CardDeck cardDeck = CardDeck.from();
+        assertEquals(cardDeck.allCard().size(), 52);
+    }
 
+    @Test
+    void 카드뽑기() {
+        CardDeck cardDeck = CardDeck.from();
+        Card card = cardDeck.drawCard();
+        assertEquals(cardDeck.allCard().size(), 51);
+    }
+
+    @Test
+    void 싱글톤_테스트() {
+        CardDeck cardDeck1 = CardDeck.from();
+        CardDeck cardDeck2 = CardDeck.from();
+        assertEquals(cardDeck1, cardDeck2);
+    }
 }
