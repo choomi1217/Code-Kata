4118a347f403f3b36f485f7b869bff509d930b10
KimWanJoo <wjwan0915@gmail.com>
2022-09-08 23:35:49 +0900

    :recycle: 이름 추가

--- blackjack/src/main/java/io/github/wjwan0/oomi/blackjack/domain/Gamer.java
+++ blackjack/src/main/java/io/github/wjwan0/oomi/blackjack/domain/Gamer.java
@@ -5,14 +5,22 @@ import java.util.List;
 public class Gamer {
 
     private final CardDeck cardDeck;
-    private int bettingMoney = 0;
+    private int money = 0;
+    private final String gamerName;
 
     private Gamer(CardDeck cardDeck) {
         this.cardDeck = cardDeck;
     }
 
-    public static Gamer from(CardDeck cardDeck) {
-        return new Gamer(cardDeck);
+    public static Gamer of(CardDeck cardDeck, String gamerName) {
+        validation(gamerName);
+        return new Gamer(cardDeck, gamerName);
+    }
+
+    private static void validation(String gamerName) {
+        if (gamerName == null || gamerName.isBlank()) {
+            throw new IllegalArgumentException("이름은 비어있을 수 없습니다.");
+        }
     }
 
     public void isWantDraw(Card card) {
@@ -32,14 +40,14 @@ public class Gamer {
     }
 
     public void bettingMoney(int inputMoney) {
-        bettingMoney = inputMoney;
+        money = inputMoney;
     }
 
     public int getMoney() {
-        return bettingMoney;
+        return money;
     }
 
-    public boolean isWantDraw(String answer) {
+    public boolean drawCard(String answer) {
         return answer.equals("y");
     }
 }
